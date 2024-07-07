d2aa6465c6ccbd9a513a1fc5b2b8bf61a28a72dd
Adam Getchell <acgetchell@ucdavis.edu>
2024-07-07 14:02:27 -0500

    feature: Use leptos

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,55 @@
-use axum::{routing::get, Router};
+// Format using leptosfmt ./**/*.rs
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+use ev::SubmitEvent;
+use leptos::*;
 
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+fn main() {
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! { <App/> }
+    })
+}
 
-    axum::serve(listener, app.into_make_service())
-        .await
-        .unwrap();
+#[allow(non_snake_case)]
+fn App() -> impl IntoView {
+    view! {
+        <h2>"Query"</h2>
+        <Query/>
+    }
+}
+
+#[allow(non_snake_case)]
+fn Query() -> impl IntoView {
+    // import the type for <input>
+    use leptos::html::Input;
+
+    let (name, set_name) = create_signal("Query".to_string());
+
+    let input_element: NodeRef<Input> = create_node_ref();
+
+    // fires when form `submit` event is triggered
+    let on_submit = move |ev: SubmitEvent| {
+        // stop the page from reloading
+        ev.prevent_default();
+
+        // here, we'll extract the value from the input
+        let value = input_element
+            .get()
+            .expect("<input> to exist")
+            .value();
+        set_name.set(value);
+    };
+
+    view! {
+        <form on:submit=on_submit>
+            <input
+                type="text"
+                value=name
+                node_ref=input_element
+                style="width: 300px; height: 40px;"
+            />
+            <input type="submit" value="Submit"/>
+        </form>
+        <p>"Name: " {name}</p>
+    }
 }
